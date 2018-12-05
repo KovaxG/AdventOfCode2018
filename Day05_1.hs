091120038c8bc922c84ba886010360aa55140078
KovaxG <georgesmth202@gmail.com>
2018-12-05 18:49:42 +0200

    Day 5 almost done first problem

--- Day05_1.hs
+++ Day05_1.hs
@@ -4,22 +4,23 @@ import Data.Char
 type Polimer = String
 
 react :: Polimer -> Polimer
-react = concat . sliding 2 r
+react = reverse . foldl rule ""
   where
-    r (a:b:[])
-      | differentCase a b && sameCharacter a b = []
-      | otherwise = [a,b]
-      
+    rule :: Polimer -> Char -> Polimer
+    rule [] c = [c]
+    rule (h:t) c
+      | differentCase c h && sameCharacter c h = t
+      | otherwise = (c:h:t)      
+  
     differentCase a b = 
       isUpper a && isLower b || isLower a && isUpper b
       
     sameCharacter a b = toUpper a == toUpper b      
 
-loop :: Polimer -> Polimer
-loop = let new = react old
-       in if new == old 
-          then new
-          else loop new
-
 calc :: [String] -> Int
-calc = length . loop . unlines
+calc = length . react . unlines
+
+main :: IO ()
+main = do
+  len <- loadDo calc "Day05_1.input"
+  putStrLn $ show len
